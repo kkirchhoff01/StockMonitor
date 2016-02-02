c800bbe2a39a35081d953bee83107806487c06d2
Kevin Kirchhoff <kkirchhoff01@yahoo.com>
2016-02-02 17:07:57 -0600

    now works as intended

--- stock_monitor.py
+++ stock_monitor.py
@@ -1,22 +1,71 @@
 import csv
 import urllib
+import time
+from matplotlib import pyplot as plt
 
 
-portfolio_log = "portfolio_log.csv"
-info_key = {}
-stock_info = {}
-with open('portfolio.csv', 'rb') as fh:
-    reader = csv.reader(fh, delimiter=',')
-    for row in reader:
-        if row[0] == "Ticker":
-            info_key[row[0]] = tuple(row[1:])
-        else:
-            stock_info[row[0]] = tuple(row[1:])
+class Monitor:
+    def __init__(self):
+        self.portfolio_log = "portfolio_log.csv"
+        self.timer = time.time()
+        self.open_hours = range(9, 15)
+        self.stock_info = {}
+        with open('portfolio.csv', 'rb') as fh:
+            reader = csv.reader(fh, delimiter=',')
+            for row in reader:
+                if row[0] == "Ticker":
+                    pass
+                else:
+                    self.stock_info[row[0]] = tuple(row[1:])
 
-def log_data(data):
-    with open(portfolio_log, 'a') as fh:
-        writer = csv.writer(fh, delimiter=',')
-        for key in data.keys():
-            writer.writerow([key] + list(data[key]))
+    def _log_data(self, log_file, data):
+        with open(log_file, 'a') as fh:
+            writer = csv.writer(fh, delimiter=',')
+            writer.writerow(data)
+            print log_file + ','.join(data)
 
-log_data(stock_info)
+    def _format_data(self, options=["s", "l1"]):
+        base_url = "http://finance.yahoo.com/d/quotes.csv?s="
+        url = "{}".format(base_url)
+        for key in self.stock_info.keys():
+            url = url + "{}+".format(key)
+        url = url[:-1] + "&f={}".format(''.join(options))
+        return url
+
+    def get_time(self):
+        return time.localtime(time.time())
+
+    def get_data(self, url):
+        current_time = self.get_time()
+        response = urllib.urlopen(url)
+        results = response.read()[:-1]
+        timestamp = time.strftime("%H:%M:%S", current_time)
+        datestamp = time.strftime("%d/%m/%Y", current_time)
+        for line in results.split('\n'):
+            info = line.split(',')
+            symbol = info[0].strip('"')
+            filename = "{}/{}.csv".format(symbol, symbol)
+            data = [datestamp, timestamp] + info[1:]
+            self._log_data(filename, data)
+            self._log_data(portfolio_log,
+                           [symbol] + [datestamp, timestamp] + info[1:],
+                           data)
+
+    def monitor_stocks(self):
+        url = self._format_data()
+        while 1:
+            current_time = self.get_time()
+            if(current_time[6] not in [5, 6] and
+                    current_time[3] in self.open_hours):
+                self.get_data(url)
+            time.sleep(600)
+
+    def plot_data(self, data):
+        plt.plot(data)
+        plt.show()
+
+if __name__ == "__main__":
+    import sys
+    sys.stdout = open('outfile.txt', 'a')
+    monitor = Monitor()
+    monitor.monitor_stocks()
